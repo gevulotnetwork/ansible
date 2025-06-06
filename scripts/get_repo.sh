ea35462c3c70c1e78f7f571acfe0cb3ee3a5690d
Brian Hechinger <wonko@4amlunch.net>
2025-06-06 11:34:45 +0100

    oh so much

--- scripts/get_repo.sh
+++ scripts/get_repo.sh
@@ -1,1 +1,4 @@
+#!/usr/bin/env bash
+
+apt install git
 git clone https://github.com/gevulotnetwork/ansible
--- scripts/host_setup.sh
+++ scripts/host_setup.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/env bash
+
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+ansible-galaxy role install -r roles/requirements.yml
+
 ansible-playbook main.yml
+pipx install --include-deps ansible
+
+python3 -m venv /root/ansible
+/root/ansible/bin/pip install -r /root/requirements.txt
